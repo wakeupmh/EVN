59b3876c5df5b5725d4995c5dca0526502aecf0b
wakeupmh <wakeupmh@gmail.com>
2020-01-21 23:30:33 -0300

    adding message and message list

---
+++ vue-test/jest-test/src/components/MessageList.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <Message :message="message" v-for="message in messages"/>
+    </ul>
+</template>
+
+<script>
+import Message from './Message'
+
+export default {
+  name: 'MessageList',
+  props: ['messages'],
+  components: {
+    Message
+  }
+}
+</script>
