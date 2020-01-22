81cd28fcd74b632b43feb6a687233d160110b1d3
wakeupmh <wakeupmh@gmail.com>
2020-01-21 23:24:58 -0300

    adding message component

---
+++ vue-test/jest-test/src/components/Message.vue
@@ -0,0 +1,10 @@
+<template>
+    <li style="padding-top: 10px" class="message">{{message}}</li>
+</template>
+
+<script>
+  export default {
+    name: 'Message',
+    props: ['message']
+  }
+</script>
