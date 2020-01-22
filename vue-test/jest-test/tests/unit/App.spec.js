3176921715083fe3c6addb302e43c66cd920ad83
wakeupmh <wakeupmh@gmail.com>
2020-01-21 23:53:31 -0300

    adding shallowMount test

---
+++ vue-test/jest-test/tests/unit/App.spec.js
@@ -0,0 +1,19 @@
+import App from '../../src/App'
+import { shallowMount } from '@vue/test-utils'
+
+describe('App.test.js',() => {
+    let component
+    
+    beforeEach(() => {
+        component = shallowMount(App)
+        component.setData({ messages: ['Dog', 'Cat', 'Fish']});
+    })
+
+    it('should equals to ["Dog", "Cat", "Fish"]', () => {
+        expect(component.vm.messages).toEqual(['Dog', 'Cat', 'Fish'])
+    })
+
+    it('has the expected html structure', () => {
+        expect(component.element).toMatchSnapshot()
+    })
+})
