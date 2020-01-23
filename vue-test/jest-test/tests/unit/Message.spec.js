2b0905fbedf56d5bb29e1f796139d17aa367cc4e
wakeupmh <wakeupmh@gmail.com>
2020-01-23 14:23:05 -0300

    testing using hasProp

--- vue-test/jest-test/tests/unit/Message.spec.js
+++ vue-test/jest-test/tests/unit/Message.spec.js
@@ -1,24 +1,25 @@
 import { mount } from "@vue/test-utils";
 import Message from "../../src/components/Message";
 
+const createCmp = propsData => mount(Message, { propsData });
+
 describe("Message.test.js", () => {
   let cmp;
-  const createCmp = propsData => mount(Message, { propsData });
 
   describe("Properties", () => {
     it("should has a message property", () => {
       cmp = createCmp({ message: "hey" });
-      expect(cmp.hasProp("message", "hey")).toBeTruthy();
+      expect(cmp.props().message).toBe("hey");
     });
 
     it("should hasn't cat property", () => {
-      cmp = createCmp({ cat: "hey" });
-      expect(cmp.hasProp("cat", "hey")).toBeFalsy();
+      cmp = createCmp({ cat: "hey", message: "hey" });
+      expect(cmp.props().cat).toBeUndefined();
     });
 
     it("should Dylan is the default author", () => {
       cmp = createCmp({ message: "hey" });
-      expect(cmp.hasProp("author", "Dylan")).toBeTruthy();
+      expect(cmp.props().author).toBe("Dylan");
     });
   });
 });
