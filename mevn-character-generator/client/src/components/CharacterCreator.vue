8572fdc941341190ee472a0684b9e8f9f6b8a0c7
wakeupmh <wakeupmh@gmail.com>
2020-03-12 17:09:57 -0300

    finishing the characterCreator component

--- mevn-character-generator/client/src/components/CharacterCreator.vue
+++ mevn-character-generator/client/src/components/CharacterCreator.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="character-creator">
     <h1>Character Creator</h1>
+    <label for="character-name">Character Name: </label>
+    <input type="text" id="character-name" v-model="name" placeholder="Enter a name" /> <br /><br />
+    <label for="professions-list">Character Profession: </label>
+    <select id="professions-list" v-model="profession">
+      <option value="Mage">Mage</option>
+      <option value="Thief">Thief</option>
+      <option value="Warrior">Warrior</option>
+    </select>
+    <p>{{name}}</p>
+    <p>{{profession}}</p>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'CharacterCreator'
+    name: 'CharacterCreator',
+    data: function () {
+      return {
+        name: "",
+        profession: ""
+      }
+    }
   }
 </script>
-
-<style scoped>
-
-</style>
