f206efcf79d17a185ffc0cdcc14a6c0075ba200f
wakeupmh <wakeupmh@gmail.com>
2020-03-12 19:12:34 -0300

    adjusting components

--- mevn-character-generator/client/src/components/CharacterViewer.vue
+++ mevn-character-generator/client/src/components/CharacterViewer.vue
@@ -1,32 +1,15 @@
 <template>
     <div class="character-viewer">
         <h1>Character Viewer</h1>
-        <p>{{characters}}</p>
+        <p v-for="(character, index) in characters" v-bind:key="index">{{character.name}} is a {{character.profession}}!</p>
     </div>
 </template>
 
 <script>
-  import axios from 'axios'
-
-  export default {
-    name: 'CharacterViewer',
-    data: function () {
-      return {
-        characters: null
-      }
-    },
-    methods: {
-      getCharacters: function () {
-        axios
-          .get('http://localhost:3000/characters')
-          .then(response => (this.characters = response.data))
-      }
-    },
-    mounted: function () {
-      this.getCharacters();
+    export default {
+        name: 'CharacterViewer',
+        props: {
+            characters: Array
+        }
     }
-  }
 </script>
-
-<style scoped>
-</style>
