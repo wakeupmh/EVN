b963f371743bcc00470e40a966cfbdbd9fbe5d13
wakeupmh <wakeupmh@gmail.com>
2020-03-14 16:29:51 -0300

    addign out-in mode to page transition

--- animating-vue/app/src/App.vue
+++ animating-vue/app/src/App.vue
@@ -4,7 +4,9 @@
       <router-link to="/">Modal</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view />
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
 
