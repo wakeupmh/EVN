fee7032facf8b61ed1ffcd6d2ea75af80069e52b
wakeupmh <wakeupmh@gmail.com>
2020-03-14 15:42:13 -0300

    1 - Transitions

--- animating-vue/app/src/App.vue
+++ animating-vue/app/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Transitions />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "./components/Transitions.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Transitions
   }
 };
 </script>
