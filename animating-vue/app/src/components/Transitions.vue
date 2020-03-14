fda9bd858164dce96897636ae376a233fecee8df
wakeupmh <wakeupmh@gmail.com>
2020-03-14 15:41:39 -0300

    learning about vue animations

---
+++ animating-vue/app/src/components/Transitions.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <button @click="toggleModal">Open</button>
+    <div v-if="isOpen" class="modal">
+      <button @click="toggleModal">Close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Transitions",
+  data() {
+    return {
+      isOpen: false
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
