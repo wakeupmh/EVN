2f06f626169751f9315f0a1ffbe23310fd452e69
wakeupmh <wakeupmh@gmail.com>
2020-03-15 12:32:45 -0300

    adjusting server and adding helmet

--- mevn-character-generator/server/app.js
+++ mevn-character-generator/server/app.js
@@ -2,16 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const server = express();
 const cors = require('cors');
-
+const helmet = require('helmet');
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to database!'));
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
 server.use(express.json());
 server.use(cors());
-
+server.use(helmet());
 server.use(`/`, require('./routes'));
 
 server.listen(3000, () => console.log("Server started!"));
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to database!'));
