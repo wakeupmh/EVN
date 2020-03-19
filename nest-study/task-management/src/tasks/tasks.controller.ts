7aebb0135b612cd85f67164dc7da98db8a5175d5
wakeupmh <wakeupmh@gmail.com>
2020-03-19 18:54:43 -0300

    adding create task dto

--- nest-study/task-management/src/tasks/tasks.controller.ts
+++ nest-study/task-management/src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task-dto';
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
@@ -11,11 +12,8 @@ export class TasksController {
   }
 
   @Post()
-  createTask(
-    @Body('title') title: string,
-    @Body('description') description: string
-  ): Task {
-    return this.tasksService.createTask(title, description);
+  async createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.tasksService.createTask(createTaskDto);
   }
   
 }
